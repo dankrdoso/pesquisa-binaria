c11eadca58b96b572cc5e10a23f7101e5827ef3d
Daniel Cardoso <142063604+dankrdoso@users.noreply.github.com>
2024-09-23 15:07:51 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         List<Integer> minhaLista = new ArrayList<>(Arrays.asList(1,3,5,7,7,9)); //a pesquisa binária só funciona em listas ordenadas.
